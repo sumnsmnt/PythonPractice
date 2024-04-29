cb7492623f918c369e10e169a97d8917d66cc5b0
Suman <sumnsmnt@gmail.com>
2024-04-29 21:03:19 +0530

    This is the first basic program pushing into Github

---
+++ welcome.py
@@ -0,0 +1,57 @@
+# Welcome to Python
+print("This is the restart of my 100 days of Python Journey.")
+
+# Let's get into variable
+name = "Suman"
+experience = 2
+
+# Print variables and data types
+print("My name is", name)
+print(type(name))
+print(type(experience))
+
+# Now jump into basic math
+# arithmatic operators
+a = 15
+b =100
+multi = a*b
+
+print(a+b)
+print(multi)
+print(b-a)
+print(b%a)
+print(b-a)
+
+# relational operators
+a=5
+b=2
+print(a == b)
+print( a != b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+# assignment operators
+num = 10
+num += 20
+print("number = ", num)
+
+# logical opreators
+print(not False)
+print(not True)
+
+a = 50
+b = 20
+print(a>b)
+print(not (a>b))
+print(a<b)
+print(not (a<b))
+
+val1 = True
+val2 = False
+print("AND Operator :", val1 and val2)
+print("OR Operator :", val1 or val2)
+
+print("AND Operator:", (a==b) and (a>b))
+print("OR Operator:", (a==b) or (a>b))
