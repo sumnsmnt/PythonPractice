2bfe0b790367b825d0b5286027e441b5bf74dd4d
Suman <sumnsmnt@gmail.com>
2024-05-01 15:49:29 +0530

    input from user

---
+++ Day2/input.py
@@ -0,0 +1,7 @@
+# print the input value from user
+name = input("enter your name please \n")
+print("Nice to meet you", name)
+
+# print the type of input value
+val = input("enter some value \n")
+print(type(val), val)
