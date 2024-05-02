f1f7771f5e05aa2f50641c45ad5a9c1be27d8415
Suman <sumnsmnt@gmail.com>
2024-05-02 21:21:59 +0530

    sum, avg & square

---
+++ Day3/average.py
@@ -0,0 +1,8 @@
+# Input 2 floating point numbers & print their average
+
+num1 = float(input("Enter First Number: "))
+num2 = float(input("Enter Second Number: "))
+
+avg = (num1+num2)/2
+
+print("The average of these 2 numbers is = ", avg)
---
+++ Day3/squareofarea.py
@@ -0,0 +1,7 @@
+# Input side of a square & print it's area
+
+side = float(input("Enter the side of a square: "))
+
+area = side*side
+
+print("Square of the area is = ", area)
