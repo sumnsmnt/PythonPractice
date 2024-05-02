5867d82624b37ba9a383efbeb5a9a13024c3d4ad
Suman <sumnsmnt@gmail.com>
2024-05-02 21:20:21 +0530

    Sum of 2 input from user

---
+++ Day3/sum.py
@@ -0,0 +1,7 @@
+# input 2 numbers & print their sum
+
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+
+sum = num1 + num2
+print("The Sum of these 2 numbers is = ", sum)
