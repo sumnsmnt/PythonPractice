1b4f14ed83750315360a691c37eca89b20865cf9
Suman <sumnsmnt@gmail.com>
2024-05-04 15:58:47 +0530

    Lists

---
+++ Day5/lists.py
@@ -0,0 +1,28 @@
+'''
+
+# List is a built in data type in Python
+marks = [90.5, 87.3, 81.8, 69.69]
+print(marks)
+print(type(marks))
+print(marks[0])
+print(len(marks))
+
+# We can store different data types in a single list
+student = ["Suman", 95, "Kolkata"]
+print(student)
+
+# Lists are mutable in nature, so we can change the value
+student[0] = "Hunter"
+print(student[0])
+print(student)
+
+# List Slicing
+marks = [96, 64, 66, 87, 99, 79]
+print(marks[1:4])
+print(marks[0:5])
+print(marks[3:])
+print(marks[-6:])
+
+'''
+
+# List Methods
